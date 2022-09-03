a98d067215b22999b1314c10884b52e7741eed20
Daniel Porto <danielportods@gmail.com>
2022-09-03 19:24:31 -0300

    Configure CI with build, test and lint

--- library_back/wait.sh
+++ library_back/wait.sh
@@ -4,18 +4,30 @@ import socket
 import time
 import os
 port = int(os.environ["POSTGRES_PORT"])
+host = os.environ["POSTGRES_HOST"]
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-soc.connect(('library_db', port))
+soc.connect((host, port))
 soc.close()
 END
 }
 
 pip3 install -r requirements.txt
 
+trys=0;
+
 until dbIsReady; do
   >&2 echo "WAITING DATABASE CONNECTION..."
   sleep 1
+
+  trys=$((trys+1))
+
+  if [ $trys -gt 10 ]
+  then
+  echo "TIME OUT!"
+  exit 1
+  fi
 done
+
 echo "DATABASE CONNECTION ESTABLISHED! :-)"
 
 python3 manage.py makemigrations
